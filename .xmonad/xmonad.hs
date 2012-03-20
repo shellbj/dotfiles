9b3acf071a28d5ae1d88791866c0bf136cd3c93e
Bryan Shell <bryan.shell@orbitz.com>
2012-03-20 17:24:00 -0500

    Fix xmonad to work xfce the way I like it

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,27 +1,36 @@
 import XMonad
-import XMonad.Hooks.ManageDocks
+import XMonad.Config.Desktop
 import XMonad.Config.Xfce
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 
-myManageHook = composeAll
-    [
-    className =? "Wrapper" --> doFloat,
-    className =? "MPlayer" --> doFloat,
-    className =? "Gimp" --> doFloat,
-    className =? "Xfce4-appfinder" --> doCenterFloat,
-    className =? "Xfrun4" --> doCenterFloat,
-    className =? "Do" --> doCenterFloat,
-    className =? "Xfce4-notes" --> doCenterFloat,
-    className =? "Xfce4-panel"    --> doFloat,
-    className =? "Xfce-mcs-manager" --> doFloat,
-    className =? "Xfce-mixer"	      --> doFloat,
-    resource  =? "desktop_window" --> doIgnore,
-    resource  =? "Xfdesktop" --> doIgnore
-    ]
+import qualified Data.Map as M
+
+myManageHook = composeAll [
+  className =? "Wrapper" --> doFloat,
+  className =? "MPlayer" --> doFloat,
+  className =? "Gimp" --> doFloat,
+  className =? "Xfce4-appfinder" --> doCenterFloat,
+  className =? "Xfrun4" --> doCenterFloat,
+  className =? "Do" --> doCenterFloat,
+  className =? "Xfce4-notes" --> doCenterFloat,
+  className =? "Xfce4-panel"    --> doFloat,
+  className =? "Xfce-mcs-manager" --> doFloat,
+  className =? "Xfce-mixer"	      --> doFloat,
+  resource  =? "desktop_window" --> doIgnore,
+  resource  =? "Xfdesktop" --> doIgnore
+  ]
 
 
-main = xmonad xfceConfig
-              {
-                manageHook = manageDocks <+> myManageHook,
-                modMask = mod4Mask
-              }
+main = xmonad $ xfceConfig {
+  manageHook = manageDocks <+> myManageHook,
+  terminal = "xfce4-terminal",
+  modMask = mod4Mask,
+  keys = \c -> myXfceKeys c `M.union` keys desktopConfig c
+  }
+
+myXfceKeys (XConfig {modMask = modm}) = M.fromList $ [
+  ((modm,               xK_p), spawn "xfrun4"),
+  ((modm .|. shiftMask, xK_p), spawn "gnome-do"),
+  ((modm .|. shiftMask, xK_q), spawn "xfce4-session-logout")
+  ]
