781724178b361f9d29f57dc9d8603d4579f97708
Bryan Shell <bryan.shell@orbitz.com>
2013-01-09 14:20:06 -0600

    Add EZConfig configuration to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Config.Desktop
 import XMonad.Config.Xfce
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Util.EZConfig
 
 import qualified Data.Map as M
 
@@ -23,14 +24,22 @@ myManageHook = composeAll [
 
 
 main = xmonad $ xfceConfig {
-  manageHook = manageDocks <+> myManageHook,
-  terminal = "xfce4-terminal",
-  modMask = mod4Mask,
-  keys = \c -> myXfceKeys c `M.union` keys desktopConfig c
+  manageHook = manageDocks <+> myManageHook
+  , terminal = "xfce4-terminal"
+  , modMask = mod4Mask
+  , keys = \c -> myXfceKeys c `M.union` keys desktopConfig c
   }
 
 myXfceKeys (XConfig {modMask = modm}) = M.fromList $ [
-  ((modm,               xK_p), spawn "xfrun4"),
-  ((modm .|. shiftMask, xK_p), spawn "gnome-do"),
-  ((modm .|. shiftMask, xK_q), spawn "xfce4-session-logout")
+  ((modm,               xK_p), spawn "xfrun4")
+  , ((modm .|. shiftMask, xK_p), spawn "gnome-do")
+  , ((modm .|. shiftMask, xK_q), spawn "xfce4-session-logout")
   ]
+
+otherKeys = \c -> mkKeymap c $[
+  ("<XF86ScreenSaver>", spawn "xscreensaver-command -lock")
+  , ("M4-p", spawn "xfrun4")
+  , ("M4-P", spawn "gnome-do")
+  , ("M4-Q", spawn "xfce4-session-logout")
+  ]
+
