1661bd19e4a466e1cc436c7f1bcd9a3cac34a45f
Bryan Shell <bryan.shell@orbitz.com>
2013-01-09 14:23:15 -0600

    Add workstation environment changes for venv, rvm and gradle

--- .zsh/env/workstation/zshenv.zsh
+++ .zsh/env/workstation/zshenv.zsh
@@ -10,8 +10,8 @@ export GROOVY_HOME=/usr/local/orbitz-groovy
 export PATH=${PATH}:${GROOVY_HOME}/bin
 
 ### Gradle
-export GRADLE_HOME=/usr/share/gradle
-export PATH=${PATH}:${GRADLE_HOME}/bin
+export GRADLE_HOME=/opt/apps/gradle-0.9
+export PATH=${GRADLE_HOME}/bin:${PATH}
 
 ### Grails
 export GRAILS_HOME=/opt/apps/grails-1.3.5
@@ -29,6 +29,14 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/oracle/11.1/client/lib
 export ORACLE_HOME="/usr/lib/oracle/xe/app/oracle/product/10.2.0/server"
 export TNS_ADMIN=${HOME}/.tnsadmin/tnsadmin
 
-export RUBYOPT="rrubygems"
+#export RUBYOPT="rrubygems"
 
 export HADOOP_HOME=/opt/apps/hadoop-0.20.2
+
+### Python
+if [[ -s /usr/bin/virtualenvwrapper.sh ]]; then
+    source /usr/bin/virtualenvwrapper.sh
+    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.6
+    export WORKON_HOME=$HOME/.virtualenvs
+    export VIRTUAL_ENV_DISABLE_PROMPT=true
+fi
