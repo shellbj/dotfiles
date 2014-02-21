c3000779e076222bb3ffb57e317784e99f0af4d9
Bryan Shell <bryan.shell@orbitz.com>
2014-02-21 11:03:01 -0600

    Use /usr/sbin/update-alternatives on ubuntu

--- .zsh/env/work-ubuntu/zshrc.zsh
+++ .zsh/env/work-ubuntu/zshrc.zsh
@@ -1,4 +1,4 @@
-alias alternatives='/usr/sbin/alternatives --admindir ~/.alternatives/admin --altdir ~/.alternatives'
+alias alternatives='/usr/sbin/update-alternatives --admindir ~/.alternatives/admin --altdir ~/.alternatives'
 compdef _update-alternatives alternatives
 
 # alias build.sh to _ant completion
