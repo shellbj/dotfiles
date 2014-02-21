919df81f0a5f95935232830536593ba9d865c019
Bryan Shell <bryan.shell@orbitz.com>
2014-02-21 10:55:25 -0600

    Add new config for ubuntu workstation

---
+++ .zsh/env/work-ubuntu/zshrc.zsh
@@ -0,0 +1,7 @@
+alias alternatives='/usr/sbin/alternatives --admindir ~/.alternatives/admin --altdir ~/.alternatives'
+compdef _update-alternatives alternatives
+
+# alias build.sh to _ant completion
+compdef _ant build.sh
+
+DEFAULT_USER=bshell
