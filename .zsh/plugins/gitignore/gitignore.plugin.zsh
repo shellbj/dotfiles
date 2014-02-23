1503afb6e860d9b6c034ff823dd847a60716dd63
Bryan Shell <bryan.shell@orbitz.com>
2014-02-22 21:05:50 -0600

    Add gitignore plugin to zsh

---
+++ .zsh/plugins/gitignore/gitignore.plugin.zsh
@@ -0,0 +1,14 @@
+function gitignore() {
+    curl http://www.gitignore.io/api/$@
+}
+
+_gitignireio_get_command_list() {
+    curl -s http://www.gitignore.io/api/list | tr "," "\n"
+}
+
+_gitignireio () {
+  compset -P '*,'
+  compadd -S '' $(_gitignireio_get_command_list)
+}
+
+compdef _gitignireio gitignore
