511371f57a308be876ff1ce0c4986f5eadec673a
Bryan Shell <bryan.shell@orbitz.com>
2014-02-22 21:09:36 -0600

    Cleanup zsh dotfiles of crufty comments

--- .zsh/lib/theme-and-appearance.zsh
+++ .zsh/lib/theme-and-appearance.zsh
@@ -2,8 +2,6 @@
 autoload -U colors
 colors
 
-#export LSCOLORS="Gxfxcxdxbxegedabagacad"
-
 unsetopt list_beep
 setopt list_packed
 setopt multios
@@ -18,12 +16,6 @@ fi
 
 # Apply theming defaults
 PS1="%n@%m:%~%# "
-
-# git theming default: Variables for theming the git info prompt
-ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
-ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
-ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
 
 # Setup the prompt with pretty colors
 setopt prompt_subst
