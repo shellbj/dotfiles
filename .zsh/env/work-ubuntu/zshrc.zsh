0b2654e703764fcfd3653a6a53e6e7979509a074
Bryan Shell <bryan.shell@orbitz.com>
2015-03-24 14:16:35 -0500

    ZSH: update some work aliases

--- .zsh/env/work-ubuntu/zshrc.zsh
+++ .zsh/env/work-ubuntu/zshrc.zsh
@@ -1,4 +1,4 @@
-alias alternatives='/usr/sbin/update-alternatives --admindir ~/.alternatives/admin --altdir ~/.alternatives'
+alias alternatives='/usr/bin/update-alternatives --admindir ~/.alternatives/admin --altdir ~/.alternatives'
 compdef _update-alternatives alternatives
 
 # alias build.sh to _ant completion
@@ -7,4 +7,3 @@ compdef _ant build.sh
 DEFAULT_USER=bshell
 
 alias fabric='bash <(curl -s "https://stash.orbitz.net/projects/CD/repos/glu-tools/browse/setup_fabric.sh?raw")'
-alias oldfabric='bash <(curl -s http://git.orbitz.net/ada/glu-tools/blobs/raw/master/setup_fabric.sh)'
