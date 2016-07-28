e1b17904ca27769e485569c460cc3ac0f6ec04bd
Bryan Shell <bshell@expedia.com>
2016-07-28 13:55:41 -0500

    [ZSH] Dump completions to hostname based file

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,7 +1,7 @@
 # add a function path
 fpath=($ZSH/functions $ZSH/completions $fpath)
 
-# Load all of the config files 
+# Load all of the config files
 for config_file ($ZSH/lib/*.zsh); do
   source $config_file
 done
@@ -24,9 +24,33 @@ for plugin ($plugins); do
     fi
 done
 
+# Figure out the SHORT hostname
+if [[ "$OSTYPE" = darwin* ]]; then
+  # OS X's $HOST changes with dhcp, etc. Use ComputerName if possible.
+  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
+else
+  SHORT_HOST=${HOST/.*/}
+fi
+
+# Save the location of the current completion dump file.
+if [ -z "$ZSH_COMPDUMP" ]; then
+  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+fi
+
 # Load and run compinit
-autoload -U compinit
-compinit -i
+autoload -U compaudit compinit
+
+if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
+  # If completion insecurities exist, warn the user without enabling completions.
+  if ! compaudit &>/dev/null; then
+
+  # Else, enable and cache completions to the desired file.
+  else
+    compinit -d "${ZSH_COMPDUMP}"
+  fi
+else
+  compinit -i -d "${ZSH_COMPDUMP}"
+fi
 
 for plugin ($plugins); do
     if [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
@@ -44,6 +68,3 @@ if [ ! "$ZSH_THEME" = ""  ]; then
         source $ZSH/themes/$ZSH_THEME.theme.zsh
     fi
 fi
-
-
-
