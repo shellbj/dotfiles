521e37285db2d7322f864c334c1f77a3bd014229
Bryan Shell <bshell@expedia.com>
2017-01-28 11:34:17 -0600

    [ZSH] Add homebrew support to nodenv plugin

--- .zsh/plugins/nodenv/nodenv.plugin.zsh
+++ .zsh/plugins/nodenv/nodenv.plugin.zsh
@@ -1,10 +1,20 @@
+_homebrew-installed() {
+  type brew &> /dev/null
+}
+
 FOUND_NODENV=0
-nodenvdirs=("$HOME/.nodenv" "/usr/local/nodenv" "/opt/nodenv")
+nodenvdirs=("$HOME/.nodenv" "/usr/local/nodenv" "/opt/nodenv" "/usr/local/opt/nodenv")
+if _homebrew-installed && nodenv_homebrew_path=$(brew --prefix nodenv 2>/dev/null); then
+    nodenvdirs=($nodenv_homebrew_path "${nodenvdirs[@]}")
+    unset nodenv_homebrew_path
+fi
 
 for nodenvdir in "${nodenvdirs[@]}" ; do
   if [ -d $nodenvdir/bin -a $FOUND_NODENV -eq 0 ] ; then
     FOUND_NODENV=1
-    export NODENV_ROOT=$nodenvdir
+    if [[ $NODENV_ROOT = '' ]]; then
+        export NODENV_ROOT=$nodenvdir
+    fi
     export PATH=${nodenvdir}/bin:$PATH
     eval "$(nodenv init - zsh)"
   fi
