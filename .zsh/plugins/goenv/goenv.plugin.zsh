931ac6d79096d465aa9ee646e9278d4ed0099020
Bryan Shell <bshell@expedia.com>
2017-01-28 11:35:05 -0600

    [ZSH] Add goenv to the env

---
+++ .zsh/plugins/goenv/goenv.plugin.zsh
@@ -0,0 +1,22 @@
+_homebrew-installed() {
+  type brew &> /dev/null
+}
+
+FOUND_GOENV=0
+goenvdirs=("$HOME/.goenv" "/usr/local/goenv" "/opt/goenv" "/usr/local/opt/goenv")
+if _homebrew-installed && goenv_homebrew_path=$(brew --prefix goenv 2>/dev/null); then
+    goenvdirs=($goenv_homebrew_path "${goenvdirs[@]}")
+    unset goenv_homebrew_path
+fi
+
+for goenvdir in "${goenvdirs[@]}" ; do
+  if [ -d $goenvdir/bin -a $FOUND_GOENV -eq 0 ] ; then
+    FOUND_GOENV=1
+    if [[ $GOENV_ROOT = '' ]]; then
+        export GOENV_ROOT=$goenvdir
+    fi
+    export PATH=${goenvdir}/bin:$PATH
+    eval "$(goenv init - zsh)"
+  fi
+done
+unset goenvdir
